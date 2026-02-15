fb57d01ee8da04ea90b4ef91a8e0cc0768e2d05c
Harshit Pranu Srivastava <108911685+Harshitpranu@users.noreply.github.com>
2026-02-15 17:16:58 +0000

    Add Django ORM models and update populate_db command for test data insertion

--- backend/octofit_tracker/management/commands/populate_db.py
+++ backend/octofit_tracker/management/commands/populate_db.py
@@ -1,57 +1,42 @@
 from django.core.management.base import BaseCommand
-from djongo import connection
+from django.core.management.base import BaseCommand
+from octofit_tracker.models import User, Team, Activity, Leaderboard, Workout
 
 class Command(BaseCommand):
     help = 'Populate the octofit_db database with test data'
 
     def handle(self, *args, **options):
-        db = connection.cursor().db_conn
-        users = db['users']
-        teams = db['teams']
-        activities = db['activities']
-        leaderboard = db['leaderboard']
-        workouts = db['workouts']
+        User.objects.all().delete()
+        Team.objects.all().delete()
+        Activity.objects.all().delete()
+        Leaderboard.objects.all().delete()
+        Workout.objects.all().delete()
 
-        # Clear collections
-        users.delete_many({})
-        teams.delete_many({})
-        activities.delete_many({})
-        leaderboard.delete_many({})
-        workouts.delete_many({})
-
-        # Insert sample users
-        users.insert_many([
-            {"name": "Iron Man", "email": "ironman@marvel.com", "team": "Marvel"},
-            {"name": "Captain America", "email": "cap@marvel.com", "team": "Marvel"},
-            {"name": "Batman", "email": "batman@dc.com", "team": "DC"},
-            {"name": "Wonder Woman", "email": "wonderwoman@dc.com", "team": "DC"}
+        User.objects.bulk_create([
+            User(name="Iron Man", email="ironman@marvel.com", team="Marvel"),
+            User(name="Captain America", email="cap@marvel.com", team="Marvel"),
+            User(name="Batman", email="batman@dc.com", team="DC"),
+            User(name="Wonder Woman", email="wonderwoman@dc.com", team="DC")
         ])
 
-        # Insert sample teams
-        teams.insert_many([
-            {"name": "Marvel", "members": ["Iron Man", "Captain America"]},
-            {"name": "DC", "members": ["Batman", "Wonder Woman"]}
+        Team.objects.bulk_create([
+            Team(name="Marvel", members=["Iron Man", "Captain America"]),
+            Team(name="DC", members=["Batman", "Wonder Woman"])
         ])
 
-        # Insert sample activities
-        activities.insert_many([
-            {"user": "Iron Man", "activity": "Running", "duration": 30},
-            {"user": "Batman", "activity": "Cycling", "duration": 45}
+        Activity.objects.bulk_create([
+            Activity(user="Iron Man", activity="Running", duration=30),
+            Activity(user="Batman", activity="Cycling", duration=45)
         ])
 
-        # Insert sample leaderboard
-        leaderboard.insert_many([
-            {"team": "Marvel", "points": 100},
-            {"team": "DC", "points": 90}
+        Leaderboard.objects.bulk_create([
+            Leaderboard(team="Marvel", points=100),
+            Leaderboard(team="DC", points=90)
         ])
 
-        # Insert sample workouts
-        workouts.insert_many([
-            {"user": "Wonder Woman", "workout": "Yoga", "duration": 60},
-            {"user": "Captain America", "workout": "Weightlifting", "duration": 40}
+        Workout.objects.bulk_create([
+            Workout(user="Wonder Woman", workout="Yoga", duration=60),
+            Workout(user="Captain America", workout="Weightlifting", duration=40)
         ])
-
-        # Create unique index on email for users
-        users.create_index("email", unique=True)
 
         self.stdout.write(self.style.SUCCESS('octofit_db database populated with test data.'))
